ac748db3563284aa532cc14e2d71052a60ecb790
sachinsingh2002 <sachinsingh2002.ssss@gmail.com>
2024-07-09 18:45:38 +0530

    update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,11 +59,10 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             </div>
             <Logo />
             <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
-              2022 Event Photos
+              2022 Marrage Photos
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              Our incredible Next.js community got together in San Francisco for
-              our first ever in-person conference!
+              A best way to remember your special day with your loved ones.
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
